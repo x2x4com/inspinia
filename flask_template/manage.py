af5c5d920a1f6c7c47c66d4116895f3fbf6205e1
Derek Miller <20miller10@gmail.com>
2016-04-17 14:18:18 -0400

    Fixing lessc app errors.

--- flask_template/manage.py
+++ flask_template/manage.py
@@ -3,6 +3,7 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 
 import os
 
+from flask_assets import ManageAssets
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
@@ -14,6 +15,7 @@ migrate = Migrate(frontend_app, db, directory=os.path.join(os.path.dirname(__fil
 manager = Manager(frontend_app)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command("assets", ManageAssets())
 manager.add_command('users', user_manager)
 manager.add_command('roles', role_manager)
 
